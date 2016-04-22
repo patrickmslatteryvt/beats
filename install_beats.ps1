c961d4239296e1ef3db10bd72fe3188b70b2bf64
Patrick M. Slattery <patrickmslatteryvt@users.noreply.github.com>
2016-04-21 21:40:38 -0400

    Enable the services
    And other minor cleanups

--- install_beats.ps1
+++ install_beats.ps1
@@ -10,14 +10,11 @@ param (
    [string]$version = "1.2.1"
 )
 
-write-output $forwarder
-write-output $version
-write-output '    hosts: ["'$forwarder':9200"]'
-
 $filebeat_yml = "C:\Program Files\Elastic\filebeat\filebeat.yml"
 $winlogbeat_yml = "C:\Program Files\Elastic\winlogbeat\winlogbeat.yml"
 $topbeat_yml = "C:\Program Files\Elastic\topbeat\topbeat.yml"
 
+# Function to unzip files (Is built-in in PS v5)
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Expand-Archive { param([string]$zipfile, [string]$outpath) [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath) }
 
@@ -55,10 +52,11 @@ $newfile = $filebeat_yml
 $text = (Get-Content -Path $oldfile -ReadCount 0) -join "`n"
 # Insert the correct FQDN to the RELK forwarder here
 $text -replace 'localhost', $forwarder | Set-Content -Path $newfile
+Remove-Item "C:\Program Files\Elastic\filebeat\filebeat.yml.temp"
 cd "C:\Program Files\Elastic\filebeat"
 PowerShell.exe -ExecutionPolicy UnRestricted -File .\install-service-filebeat.ps1
-# Set-Service filebeat -startuptype automatic
-# Start-Service -name filebeat
+Set-Service filebeat -startuptype automatic
+Start-Service -name filebeat
 
 # Install and enable Winlogbeat
 Start-BitsTransfer -Source https://download.elastic.co/beats/winlogbeat/winlogbeat-$version-windows.zip -Destination "C:\Program Files\Elastic"
@@ -74,8 +72,8 @@ $text -replace 'localhost:9200', 'relkfwdofg.internal.mywebgrocer.com:9200' | Se
 $text -replace 'localhost:9200', "$(Get-Date):9200" | Set-Content -Path $newfile
 cd "C:\Program Files\Elastic\winlogbeat"
 PowerShell.exe -ExecutionPolicy UnRestricted -File .\install-service-winlogbeat.ps1
-# Set-Service winlogbeat -startuptype automatic
-# Start-Service -name winlogbeat
+Set-Service winlogbeat -startuptype automatic
+Start-Service -name winlogbeat
 
 # Install but disable topbeat
 Start-BitsTransfer -Source https://download.elastic.co/beats/topbeat/topbeat-$version-windows.zip -Destination "C:\Program Files\Elastic"
@@ -85,5 +83,5 @@ Rename-Item -path "C:\Program Files\Elastic\topbeat-$version-windows" -newName "
 ### Need to write out the YAML file here
 cd "C:\Program Files\Elastic\topbeat"
 PowerShell.exe -ExecutionPolicy UnRestricted -File .\install-service-topbeat.ps1
-# Set-Service topbeat -startuptype disabled
-# Stop-Service -name topbeat
+Set-Service topbeat -startuptype disabled
+Stop-Service -name topbeat
